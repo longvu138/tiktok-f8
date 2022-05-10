3fcc75acb23da935a04ff505eb860db4f7e6b0fb
longvu138 <tranlongvu138@gmail.com>
2022-05-10 09:58:25 +0700

    setup Router, Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="App">
-            <h1> React app nè</h1>
-        </div>
+        // tạo Router bọc ngoài
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        /*
+                        // nếu trong route không truyền vào props layout 
+                        //thì lấy layout mặc định là default layout
+                        
+                        // nếu layout nhập vào bằng null thì trả về Fragment
+                        // còn ngược lại trả về Default Layout
+                        */
+
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    // page trở thành children của layout
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
